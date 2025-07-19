f689df050d6b899924e5216c8ad550945bfa4c98
User <z;dfokrj@dfkh>
2025-07-19 11:29:27 +0300

    cvcod

--- CVcod.py
+++ CVcod.py
@@ -1,26 +1,47 @@
-import cv2
 import numpy as np
-#include <opencv2/objdetect/aruco_detector.hpp>
+import cv2 as cv
 
-import cv2
-import argparse
+cap = cv.VideoCapture(0)
 
-cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
-while True:
+while cap.isOpened():
     ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    frame = cv.flip(frame, 0)
 
+    # write the flipped frame
     out.write(frame)
 
-    cv2.imshow('video feed', frame)
+    cv.imshow('frame', frame)
 
+    ##########################
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    aruco_dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_250)
+    parameters = cv.aruco.DetectorParameters()
+    aruco_dict_2 = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_5X5_250)
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    parameters2 = cv.aruco.DetectorParameters()
+
+    # Create the ArUco detector
+    detector = cv.aruco.ArucoDetector(aruco_dict, parameters)
+    # Detect the markers
+    corners, ids, rejected = detector.detectMarkers(gray)
+
+    detector_2 = cv.aruco.ArucoDetector(aruco_dict_2, parameters2)
+    # Detect the markers
+    corners, ids, rejected = detector.detectMarkers(gray)
+
+    print("Detected markers:", ids)
+    if ids is not None:
+        cv.aruco.drawDetectedMarkers(frame, corners, ids)
+        cv.imshow('Detected Markers', frame)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
 
